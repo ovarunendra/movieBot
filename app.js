8a0bcc22af6130a6777d0a028c73076af3b40732
ovarunendra <vpsingh016@gmail.com>
2017-01-26 21:52:22 +0530

    URL fix

--- app.js
+++ app.js
@@ -144,8 +144,8 @@ function getMovieDetail(userId, field) {
 }
 
 function findMovie(userId, movieTitle) {
-  request("http://www.omdbapi.com/?type=movie&amp;t=" + movieTitle, function (error, response, body) {
-    if (!error &amp;&amp; response.statusCode === 200) {
+  request("http://www.omdbapi.com/?type=movie&t=" + movieTitle, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
       var movieObj = JSON.parse(body);
       if (movieObj.Response === "True") {
         var query = {user_id: userId};
